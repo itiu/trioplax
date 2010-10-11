a611b4a9284e5e685807221a3cf9c5dc4127bf31
Itiu <ValeriyBushenev@gmail.com>
2010-10-12 01:28:39 +0400

    +1

--- build_with_dmd.sh
+++ build_with_dmd.sh
@@ -2,8 +2,10 @@ rm *.a
 src=src/trioplax/memory
 general_src=src/trioplax
 
+git log -1 --pretty=format:"module myversion; public final static char[] author=\"%an\"; public final static char[] hash=\"%h\";">myversion.d
+
 dmd -Iimport/libmongod $general_src/Log.d $general_src/triple.d $general_src/TripleStorage.d $src/Hash.d $src/TripleHashMap.d $src/IndexException.d $src/TripleStorageMemory.d \
-$general_src/mongodb/TripleStorageMongoDB.d -O -Hdexport/trioplax -release -lib -oftrioplax
+$general_src/mongodb/TripleStorageMongoDB.d myversion.d -O -Hdexport/trioplax -release -lib -oftrioplax
 
 
 rm *.o
